b4066ff50c366705e406061f703ee1d742177584
raunaq sayyed <raunaqsayyed7@gmail.com>
2026-01-14 03:48:19 +0530

    Add comments for Article Views I SQL query
    Added comments for LeetCode problem details and SQL query.

--- problems/1148_Article_Views_I.sql
+++ problems/1148_Article_Views_I.sql
@@ -1,1 +1,10 @@
+-- LeetCode Problem: 1148. Article Views I
+-- Difficulty: Easy
+-- Concept: DISTINCT, filtering, ORDER BY
+-- Link: https://leetcode.com/problems/article-views-i/
+
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC;
 
