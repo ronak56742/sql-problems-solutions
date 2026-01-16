f533c267869c239b8f923475e573b9c251d8f3fe
raunaq sayyed <raunaqsayyed7@gmail.com>
2026-01-17 05:04:52 +0530

    Create 1068_Product_Sales_Analysis_I.sql

---
+++ problems/1068_Product_Sales_Analysis_I.sql
@@ -0,0 +1,12 @@
+-- LeetCode Problem: 1068. Product Sales Analysis I
+-- Difficulty: Easy
+-- Concept: INNER JOIN
+-- Link: https://leetcode.com/problems/product-sales-analysis-i/
+
+SELECT 
+    p.product_name,
+    s.year,
+    s.price
+FROM Sales s
+INNER JOIN Product p
+ON s.product_id = p.product_id;
